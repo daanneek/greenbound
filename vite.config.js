52b1992386d426d91e55e26d0f652aaf31af38fa
Daan <daan+github@nkmn.nl>
2026-09-12 19:02:22 +0200

    fix import

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
+import react from "@vitejs/plugin-react";
 import { copyFileSync } from "node:fs";
 import { resolve } from "node:path";
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 function copyMapLibreWorkerShared() {
